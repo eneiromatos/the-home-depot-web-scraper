243e8dd37ad21569fb144be37189bb85501d8fa6
Eneiro Matos <eneiromatos@gmail.com>
2022-08-03 03:12:50 -0400

    Add reziseVieport, getItems, getPagination

--- src/routes.ts
+++ src/routes.ts
@@ -5,8 +5,41 @@ import { allPages, startPage, lastPage } from "./main.js";
 export const router = createPuppeteerRouter();
 
 router.addHandler(labels.listing, async ({ request, page, log }) => {
-  const title = await page.title();
-  log.info(`Handle pagination: ${title} [${request.loadedUrl}]`);
+  async function reziseVieport() {
+    const bodyWidth = 1440;
+    const bodyHeight = 40000;
+    await page.setViewport({ width: bodyWidth, height: bodyHeight });
+    await page.keyboard.press("End");
+    await page.waitForSelector("#footerTagline");
+  }
+
+  async function getItems() {
+    const selector =
+      'div.results-wrapped div[data-lg-name*="Product Pod"] div.product-pod__title a';
+
+    const itemUrls = await page.$$eval(selector, (urls) =>
+      urls.map((url) => url.getAttribute("href"))
+    );
+    return itemUrls;
+  }
+
+  async function getPagination() {
+    const numItemsRange = await page.$eval(
+      "div.results-pagination__counts span:nth-of-type(1)",
+      (el) => el.textContent
+    );
+    const totalItems = Number(
+      await page.$eval(
+        "div.results-pagination__counts span:nth-of-type(2)",
+        (el) => el.textContent
+      )
+    );
+    const numItems = Number(numItemsRange?.split("-")[1]);
+    const maxPages = Math.ceil(totalItems / numItems);
+    0 == 0;
+  }
+  await reziseVieport();
+  await getPagination();
 });
 
 router.addHandler(labels.detail, async ({ request, page, log }) => {
